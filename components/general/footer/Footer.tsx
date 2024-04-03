3566b0d29081a7c46c290996657219a47517e7a3
Duyil Ayomid <isaacseun63@gmail.com>
2024-04-03 18:43:34 +0100

    chores: made update on the footer section

--- components/general/footer/Footer.tsx
+++ components/general/footer/Footer.tsx
@@ -5,16 +5,15 @@ import {
   otherServiceRoutes,
   ourServiceRoutes,
   aboutRoutes,
-  policiesRoutes,  
-  siteName,
+  policiesRoutes,
+  siteName
 } from "@/utils/_variables";
 import React from "react";
 import Link from "next/link";
 
-
 const FooterContent: React.FC<{
   title?: string;
-  content: { label: string; path: string }[]; 
+  content: { label: string; path: string }[];
 }> = ({ title, content }) => {
   return (
     <div data-aos="fade-up" className="flex flex-col gap-6">
@@ -42,26 +41,36 @@ const Footer = () => {
 
   return (
     <SectionContainer className="bg-primary-30 py-10 gap-10 flex flex-col">
-       <div className="flex justify-center gap-6 mt-4">
-          <div className="flex justify-center bg-green-700 w-full p-4">
-            <p className="text-white text-lg font-bold">Cars on auction</p>
-          </div>
-          <div className="flex justify-center bg-green-700 w-full p-4">
-            <p className="text-white text-lg font-bold">Cars for fixed price </p>
-          </div>
-          <div className=" border border-green-800 flex justify-center w-full p-4">
-            <p className="text-green-600 text-lg font-bold">Sell car</p>
-          </div>
-       </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
+        <Link
+          href=""
+          className="flex justify-center bg-primary rounded-md w-full p-4"
+        >
+          <p className="text-white text-lg font-bold">Cars on auction</p>
+        </Link>
+        <Link
+          href=""
+          className="flex justify-center bg-primary rounded-md w-full p-4"
+        >
+          <p className="text-white text-lg font-bold">Cars for fixed price </p>
+        </Link>
+        <Link
+          href=""
+          className=" border border-primary flex justify-center rounded-md w-full p-4"
+        >
+          <p className="text-primary text-lg font-bold">Sell car</p>
+        </Link>
+      </div>
       <div className="flex items-start gap-6 justify-between flex-wrap pb-10">
         <FooterContent title="Other services" content={otherServiceRoutes} />
         <FooterContent title="Our services" content={ourServiceRoutes} />
         <FooterContent title="About Akandi online" content={aboutRoutes} />
-        <FooterContent title="Policies and condition" content={policiesRoutes} />
-      
+        <FooterContent
+          title="Policies and condition"
+          content={policiesRoutes}
+        />
       </div>
       <div className="flex justify-end w-full pt-4 border-t">
-
         <ul className="flex items-center flex-wrap gap-6">
           {allSocials.map(({ label, link }, index) => (
             <li key={index}>
@@ -78,9 +87,12 @@ const Footer = () => {
         </ul>
       </div>
       <div className="flex gap-5 items-center justify-center  w-full pt-4">
-        <p className="text-center text-lg font-light text-slate-800 italic"> copyright &copy; {date.getFullYear()} Akandi online.com. All right reserved.</p>
+        <p className="text-center text-lg font-light text-slate-800 italic">
+          {" "}
+          copyright &copy; {date.getFullYear()} Akandi online.com. All right
+          reserved.
+        </p>
       </div>
-     
     </SectionContainer>
   );
 };
